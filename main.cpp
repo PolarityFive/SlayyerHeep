4cedb65b78e6d91ba51308c885e57d28c9dda813
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2018-05-25 03:21:53 +0300

    Update main.cpp
    Fixed comments appearing weirdly.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include <iostream>													// Like I said below, all feedback is appreciated and taken into consideration.
+#include <iostream>							// Like I said below, all feedback is appreciated and taken into consideration.
 #include <cstdlib>
 #include <Windows.h>
 #include <stdio.h>
 #include "cheat.h"
 using namespace std;
-bool fireStatus = false, knifeStatus = false;						//Global Bool functions to control on/off switches on Rapid Fire and Rapid knife cheats. 
-void displayMenu();													//The menu displaying function.  It is located in the toggle.cpp file. 
+bool fireStatus = false, knifeStatus = false;				//Global Bool functions to control on/off switches on Rapid Fire and Rapid knife cheats. 
+void displayMenu();							//The menu displaying function.  It is located in the toggle.cpp file. 
 bool toggleFunction(HANDLE hProcess, int hNumber, int32_t pLayer);
 
-DWORD WINAPI RapidFire(LPVOID cheatOBJ);							 //Thread functions. Defined below main.
+DWORD WINAPI RapidFire(LPVOID cheatOBJ);		//Thread functions. Defined below main.
 DWORD WINAPI RapidKnife(LPVOID cheatOBJ);
 
 
-cheat* cheatObj = new cheat;										//Global Object, because I gotta use it in Thread Functions below.  I'm not sure if this is a good solution at the problem I had. 
+cheat* cheatObj = new cheat;		//Global Object, because I gotta use it in Thread Functions below.  I'm not sure if this is a good solution at the problem I had. 
 int main()
 {
 	int32_t pLayer;
@@ -21,11 +21,11 @@ int main()
 
 	cheatObj->GetSetHandle();
 	ReadProcessMemory(cheatObj->hProcess, (LPCVOID)(cheatObj->BaseAdress + cheatObj->offsetToLocalPlayer), &pLayer, sizeof(cheatObj->pLayer), NULL); //Dereferencing pointers to Offset to the local player
-	cheatObj->pLayer = pLayer;									//Seding pLayer address to the Object pLayer 										//For easier access inside main function.
+	cheatObj->pLayer = pLayer;//Seding pLayer address to the Object pLayer 										//For easier access inside main function.
 	displayMenu();
 	do
 	{
-		if (GetAsyncKeyState(0x74) & 1)								//0x74 == F5 key.
+		if (GetAsyncKeyState(0x74) & 1)							//0x74 == F5 key.
 			toggleFunction(cheatObj->hProcess, 1, pLayer);
 		else if (GetAsyncKeyState(0x75) & 1)						//0x75 == F6 key.
 			toggleFunction(cheatObj->hProcess, 2, pLayer);
@@ -56,7 +56,7 @@ int main()
 
 	cout << "Thank you for trying out my first project. All feedback is appreaciated and" << endl << "taken into consideration." << endl;
 
-	cheatObj->closehandle();				//Closing Handle. 
+	cheatObj->closehandle();	//Closing Handle. 
 	system("PAUSE");
 	return(0);
 }
@@ -64,14 +64,14 @@ int main()
 
 DWORD WINAPI RapidFire(LPVOID Param)
 {
-	while (fireStatus == true)				//This is the RapidFire toggle check
+	while (fireStatus == true)	//This is the RapidFire toggle check
 	{
 		cheatObj->rapidFire(cheatObj);
 	}
 	return(0);
 }
 
-DWORD WINAPI RapidKnife(LPVOID Param)		//This is the RapidKnife toggle check. 
+DWORD WINAPI RapidKnife(LPVOID Param)	//This is the RapidKnife toggle check. 
 {
 	while (knifeStatus == true)
 	{
